c512fc00da578f1623579186507592f057e83aae
Archana-naik0019 <archana.naik@students.iiserpune.ac.in>
2025-05-14 00:22:24 +0530

    Create ActionInitialization.cc

---
+++ ActionInitialization.cc
@@ -0,0 +1,18 @@
+#include "ActionInitialization.hh"
+#include "PrimaryGeneratorAction.hh"
+#include "RunAction.hh"
+#include "EventAction.hh"
+
+ActionInitialization::ActionInitialization() {}
+ActionInitialization::~ActionInitialization() {}
+
+void ActionInitialization::Build() const {
+    SetUserAction(new PrimaryGeneratorAction());
+    SetUserAction(new RunAction());
+    SetUserAction(new EventAction());
+}
+
+void ActionInitialization::BuildForMaster() const {
+    SetUserAction(new RunAction());
+    SetUserAction(new EventAction());
+}
